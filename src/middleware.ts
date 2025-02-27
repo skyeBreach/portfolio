972e37ffabb868ead14ae95430d6eede5c862a15
skyeBreach <skyebenson@hotmail.com>
2025-02-27 03:52:30 +0000

    feat: add initial middleware implementation

---
+++ src/middleware.ts
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export const middleware = (request: NextRequest) => {
+
+};
+
+export const config = {};
